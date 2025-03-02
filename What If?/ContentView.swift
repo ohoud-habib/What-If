01b55e43d814dac8855931afab46ccd3fec3e25f
ohoud-habib <ohoudhabib@gmail.com>
2025-03-02 12:36:07 +0300

    Initial Commit

---
+++ What If?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  What If?
+//
+//  Created by ohoud on 02/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ What If?/What_If_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_If_App.swift
+//  What If?
+//
+//  Created by ohoud on 02/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct What_If_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
