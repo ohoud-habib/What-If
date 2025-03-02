d5182e2acc41c328149e13ca8a290c8ec99cdddf
ohoud-habib <ohoudhabib@gmail.com>
2025-03-02 13:28:51 +0300

    ..

---
+++ What If?/test.swift
@@ -0,0 +1,24 @@
+//
+//  test.swift
+//  What If?
+//
+//  Created by ohoud on 02/09/1446 AH.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("--------")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+   test()
+}
